0fe7fd431b929ad1f5c66649b5713f401d0add9d
nguyenvantoan1821051127 <79552710+nguyenvantoan1821051127@users.noreply.github.com>
2022-11-19 15:50:55 +0700

    update index

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "cannot-be-guess"
     SQLALCHEMY_DATABASE_URI = (
-        "postgresql://postgres:200420@localhost:5432/ProjectCNTTDH"
+        "postgresql://postgres:nguyentoan@localhost:5432/Cimera"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     print("Connect to database successfully!")

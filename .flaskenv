898ab1668706fecb001f3b69a2ddff56e4af9158
VanViet200420 <viet24hol@gmail.com>
2022-11-30 09:35:09 +0700

    CRUD suppllier, movie, cinema

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,4 @@
 flask_app = application.py
 flask_debug = 1
+flask_run_host=localhost
+flask_run_port=8080
--- app/models.py
+++ app/models.py
@@ -8,15 +8,15 @@ from datetime import datetime
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = "users"
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
-    phone_number = db.Column(db.String(10), nullable=False, unique=True)
-    gender = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String(10), unique=True)
+    gender = db.Column(db.String)
     is_admin = db.Column(db.Boolean, default=False)
-    evaluates = db.relationship("Evaluate", back_populates="user")
+    evaluates = db.relationship('Evaluate', back_populates='user')
 
     def set_password(self, password_input):
         self.password = generate_password_hash(password_input)
@@ -30,74 +30,82 @@ class User(db.Model, UserMixin):
 
 
 class Cinema(db.Model):
-    __tablename__ = "cinemas"
+    __tablename__ = 'cinemas'
     id = db.Column(db.Integer, primary_key=True)
-    id_supplier = db.Column(db.Integer, db.ForeignKey("suppliers.id"), nullable=False)
+    id_supplier = db.Column(db.Integer, db.ForeignKey(
+        'suppliers.id'), nullable=False)
     name = db.Column(db.String(255), nullable=False)
     address = db.Column(db.String(255), nullable=False)
+    district =db.Column(db.String(255), nullable=False)
     hotline = db.Column(db.String(20))
-    geom = db.Column(Geometry("POINT"))
+    geom = db.Column(Geometry('POINT'))
     movie_showtimes = db.relationship(
-        "Movie_showtime", back_populates="cinema", cascade="all,delete"
+        'Movie_showtime', back_populates='cinema', cascade='all,delete'
     )
     evaluates = db.relationship(
-        "Evaluate", back_populates="cinema", cascade="all,delete"
+        'Evaluate', back_populates='cinema', cascade='all,delete'
     )
     supplier = db.relationship(
-        "Supplier", back_populates="cinema", cascade="all,delete"
+        'Supplier', back_populates='cinema', cascade='all,delete'
     )
 
 
 class Movie(db.Model):
-    __tablename__ = "movies"
+    __tablename__ = 'movies'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     img = db.Column(db.String, nullable=False)
     describe = db.Column(db.Text)
-    director = db.Column(db.String(50), nullable=False)
-    actor = db.Column(db.Text, nullable=False)
-    genre = db.Column(db.Text, nullable=False)
+    director = db.Column(db.String(50))
+    actor = db.Column(db.Text)
+    genre = db.Column(db.Text)
     release_date = db.Column(db.String(20), nullable=False)
     running_time = db.Column(db.String(50), nullable=False)
     language = db.Column(db.Text)
     rated = db.Column(db.Text)
     movie_showtimes = db.relationship(
-        "Movie_showtime", back_populates="movie", cascade="all,delete"
+        'Movie_showtime', back_populates='movie', cascade='all,delete'
     )
 
 
 class Movie_showtime(db.Model):
-    __tablename__ = "movie_showtimes"
+    __tablename__ = 'movie_showtimes'
     id = db.Column(db.Integer, primary_key=True)
-    id_cinema = db.Column(db.Integer, db.ForeignKey("cinemas.id"), nullable=False)
-    id_movie = db.Column(db.Integer, db.ForeignKey("movies.id"), nullable=False)
+    id_cinema = db.Column(db.Integer, db.ForeignKey(
+        'cinemas.id'), nullable=False)
+    id_movie = db.Column(db.Integer, db.ForeignKey(
+        'movies.id'), nullable=False)
     screening_date = db.Column(db.String(50), nullable=False)
     time_start = db.Column(db.String(20))
     seats = db.Column(db.Integer)
     cinema = db.relationship(
-        "Cinema", back_populates="movie_showtimes", cascade="all,delete"
+        'Cinema', back_populates='movie_showtimes', cascade='all,delete'
     )
     movie = db.relationship(
-        "Movie", back_populates="movie_showtimes", cascade="all,delete"
+        'Movie', back_populates='movie_showtimes', cascade='all,delete'
     )
 
 
 class Evaluate(db.Model):
-    __tablename__ = "evaluates"
+    __tablename__ = 'evaluates'
     id = db.Column(db.Integer, primary_key=True)
-    id_user = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    id_cinema = db.Column(db.Integer, db.ForeignKey("cinemas.id"), nullable=False)
+    id_user = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    id_cinema = db.Column(db.Integer, db.ForeignKey(
+        'cinemas.id'), nullable=False)
     content = db.Column(db.Text)
     star_rated = db.Column(db.Integer)
     created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
-    user = db.relationship("User", back_populates="evaluates", cascade="all,delete")
-    cinema = db.relationship("Cinema", back_populates="evaluates", cascade="all,delete")
+    user = db.relationship(
+        'User', back_populates='evaluates', cascade='all,delete')
+    cinema = db.relationship(
+        'Cinema', back_populates='evaluates', cascade='all,delete')
 
 
 class Supplier(db.Model):
-    __tablename__ = "suppliers"
+    __tablename__ = 'suppliers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     img_price_ticket = db.Column(db.String, nullable=False)
-    cinema = db.relationship("Cinema", back_populates="supplier", cascade="all,delete")
+    cinema = db.relationship(
+        'Cinema', back_populates='supplier', cascade='all,delete')
